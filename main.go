f56a142183f58991309476b3f2cf27af51266d26
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-10-20 22:47:12 -0300

    chore: initialize load dotenv file

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/codeferreira/jwt-auth-with-go/initializers"
+)
+
+func init() {
+	initializers.LoadEnvVariables()
+}
 
 func main() {
 	fmt.Println("Hello World!")
