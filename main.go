4beae6af02261548ee59788cdd4c77da25929266
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-10-23 16:33:29 -0300

    feat: add db initialization and signup route

--- main.go
+++ main.go
@@ -4,12 +4,15 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/codeferreira/jwt-auth-with-go/controllers"
 	"github.com/codeferreira/jwt-auth-with-go/initializers"
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
 	initializers.LoadEnvVariables()
+	initializers.ConnectToDb()
+	initializers.SyncDatabase()
 }
 
 func main() {
@@ -20,6 +23,8 @@ func main() {
 			"message": "pong",
 		})
 	})
+
+	router.POST("/signup", controllers.Signup)
 
 	router.Run(os.Getenv("PORT"))
 }
