87214da0cd3b77c3a99d49c260ba5059edde5b1e
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-10-24 11:37:21 -0300

    feat: add auth validation middleware

---
+++ middleware/requireAuth.go
@@ -0,0 +1,49 @@
+package middleware
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/codeferreira/jwt-auth-with-go/initializers"
+	"github.com/codeferreira/jwt-auth-with-go/models"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func RequireAuth(context *gin.Context) {
+	tokenString, err := context.Cookie("Authorization")
+
+	if err != nil {
+		context.AbortWithStatus(http.StatusUnauthorized)
+	}
+	
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+	
+		return []byte(os.Getenv("JWT_SECRET")), nil
+	})
+	
+	if claims, ok := token.Claims.(jwt.MapClaims); ok && token.Valid {
+		if float64(time.Now().Unix()) > claims["exp"].(float64) {
+			context.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		var user models.User
+
+		initializers.DB.First(&user, claims["sub"])
+
+		if user.ID == 0 {
+			context.AbortWithStatus(http.StatusUnauthorized)
+		}
+
+		context.Set("user", user)
+
+		context.Next()
+	} else {
+		context.AbortWithStatus(http.StatusUnauthorized)
+	}
+}
