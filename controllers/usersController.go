49e3136bb4f49ff66ab780580874e30a734550d9
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2022-10-24 11:13:36 -0300

    feat: add login controller

--- controllers/usersController.go
+++ controllers/usersController.go
@@ -2,10 +2,13 @@ package controllers
 
 import (
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/codeferreira/jwt-auth-with-go/initializers"
 	"github.com/codeferreira/jwt-auth-with-go/models"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v4"
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -38,7 +41,6 @@ func Signup(context *gin.Context) {
 	result := initializers.DB.Create(&user)
 
 	if result.Error != nil {
-
 		context.JSON(http.StatusBadRequest, gin.H{
 			"error": "Failed to create the user",
 		})
@@ -47,4 +49,63 @@ func Signup(context *gin.Context) {
 	}
 
 	context.JSON(http.StatusCreated, user)
+}
+
+
+func Login(context *gin.Context) {
+	var body struct {
+		Name     string
+		Email    string
+		Password string
+	}
+
+	if context.Bind(&body) != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to read body",
+		})
+
+		return
+	}
+
+	var user models.User
+	initializers.DB.First(&user, "email = ?", body.Email)
+
+	if user.ID == 0 {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+
+		return
+	}
+
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(body.Password))
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Invalid email or password",
+		})
+
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"sub": user.ID,
+		"exp": time.Now().Add(time.Hour * 24 * 3).Unix(),
+	})
+
+	tokenString, err := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+
+	if err != nil {
+		context.JSON(http.StatusBadRequest, gin.H{
+			"error": "Failed to authenticate",
+		})
+
+		return
+	}
+
+	context.SetSameSite(http.SameSiteLaxMode)
+	context.SetCookie("Authorization", tokenString, 3600 * 24 * 3, "", "", false, true)
+
+	context.JSON(http.StatusOK, gin.H{
+	})
 }
